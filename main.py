82a34c8027f041426cafa31ca2284f7d29235e86
Wei-Liang <wl.chew.wl@gmail.com>
2014-10-14 15:55:03 +0800

    WIP: added code for running from the  command line

---
+++ main.py
@@ -0,0 +1,22 @@
+# Import libraries
+import sys
+
+def doStuff(filein,fileout):
+    # open file handles to both files
+    fin  = open(filein)       # by default, read mode
+    fout = open(fileout,'w')  # write mode
+    c    = fin.read()         # read in file into c
+
+    # now put your code: convert every second character to upper case
+
+    # and write to fileout
+
+
+# our main function
+if __name__=="__main__":
+
+    # parse our arguments
+
+
+    # all done
+
