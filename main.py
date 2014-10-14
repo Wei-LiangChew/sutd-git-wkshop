c8f50a41a1cc96c80ec7869e244bfbc39d006b25
Wei-Liang <wl.chew.wl@gmail.com>
2014-10-14 16:06:26 +0800

    removed unecessary code and added argument checks

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 # Import libraries
 import sys
+# from parser.py import *
 
-def doStuff(filein,fileout):
-    # open file handles to both files
-    fin  = open(filein)       # by default, read mode
-    fout = open(fileout,'w')  # write mode
-    c    = fin.read()         # read in file into c
-
-    # now put your code: convert every second character to upper case
-
-    # and write to fileout
-
+MONDAY = "Monday"
+TUESDAY = "Tuesday"
+WEDNESDAY = "Wednesday"
+THURSDAY = "Thursday"
+FRIDAY = "Friday"
+DAYS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY]
 
 # our main function
 if __name__=="__main__":
-
     # parse our arguments
 
+    if len(sys.argv) < 3:
+        print "Error: No of arguments supplied", len(sys.argv) - 1
+        print "Expected: day, time"
+    else:
+        day = sys.argv[1]
+        time = sys.argv[2]
+        data = parse()
 
+        rooms_available = get_available_rooms(day, time, data)
+
+        print rooms_available
     # all done
+
+
+def get_available_rooms(day, time, data):
+    if day not in DAYS:
+        return "Error: invalid day: %s" % day
+    if validate_time(time) == False:
+        return "Error: invalid time: %s" % time
+
+
+
+
+def validate_time(time):
+    time_data = time.split(':')
+    hour = int(time_data[0])
+    minute = int(time_data[1])
+
+    if 0 < hour and hour < 24 and 0 < minute and minute < 60:
+        return True
+    else:
+        return False
 
